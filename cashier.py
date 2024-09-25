19fa3eab0011aa778ca164448a1d124e1851b939
JackK1014 <jking1014456@gmail.com>
2024-09-25 18:01:19 -0400

    feat: moved the process_coins and transaction_result functions to their own file

---
+++ cashier.py
@@ -0,0 +1,24 @@
+class Cashier:
+    def __init__(self):
+        pass
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted.
+           Hint: include input() function here, e.g. input("how many quarters?: ")"""
+        large_dollars = int(input("how many large dollars?: "))
+        half_dollars = int(input("how many half dollars?: "))
+        quarters = int(input("how many quarters?: "))
+        nickels = int(input("how many nickels?: "))
+
+        return large_dollars + half_dollars * .5 + quarters * .25 + nickels * .05
+
+    def transaction_result(self, coins, cost):
+        """Return True when the payment is accepted, or False if money is insufficient.
+           Hint: use the output of process_coins() function for cost input"""
+        if coins < cost:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
+        else:
+            change = coins - cost
+            print(f"Here is ${change} in change.")
+        return True
